7110f545ba0f26588afdab8651f73f86a1bccb2f
bebinPaula <azzar.arza18@gmail.com>
2022-12-12 21:35:41 +0700

    update html

--- tampilHome.js
+++ tampilHome.js
@@ -3,7 +3,7 @@ let divData = document.getElementById("divData");
 getUser();
 //get
 function getUser() {
-  fetch("http://localhost:3003/ternak")
+  fetch("https://if3b-zarazzar.vercel.app/ternak") //https://if3b-zarazzar.vercel.app/ternak .. fetch("http://localhost:3003/ternak")
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
