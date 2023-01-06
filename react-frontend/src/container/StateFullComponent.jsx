0681f0ff943104505182817eb5a44b30b5b67021
zarazzar <azzar.arza18@gmail.com>
2023-01-07 00:54:41 +0700

    updet

--- react-frontend/src/container/StateFullComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class StateFullComponent extends React.Component {
-  render() {
-    return <p>haii ini statefulll Component</p>;
-  }
-}
-
-export default StateFullComponent;
